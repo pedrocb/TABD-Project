1f01d536a50cec8f2f0642998ea14f16e20d5849
Pedro Belém <pedrocb_c-s@hotmail.com>
2017-05-17 13:40:33 +0100

    create and fill time table

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 import psycopg2 as psy
 
-conn = psy.connect("dbname=guest user=guest")
-
 def create_time_table(conn):
     cur = conn.cursor()
-    cur.execute("CREATE TABLE TEMPO ()")
+    cur.execute("create table Timestamps ( TIMESTAMP timestamp, ID serial primary key);")
+    conn.commit()
+
+def fill_time_table(conn):
+   cur = conn.cursor()
+   cur.execute('''insert into timestamps
+                select timestamp from generate_series(
+                  '2015-01-01 12:00am'::date,
+                  '2015-09-23 11:00pm'::date,
+                  '1 hour'::interval) timestamp;''')
+   conn.commit()
+
+if __name__ == "__main__":
+    conn = psy.connect("dbname=guest user=guest")
+    create_time_table(conn)
+    fill_time_table(conn)

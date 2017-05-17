1ba0610b3b978dc3b90d276bc14bd05c29ef98f5
Pedro Belém <pedrocb_c-s@hotmail.com>
2017-05-17 16:49:14 +0100

    creates and fills locations table

--- setup.py
+++ setup.py
@@ -8,13 +8,29 @@ def create_time_table(conn):
 def fill_time_table(conn):
    cur = conn.cursor()
    cur.execute('''insert into timestamps
-                select timestamp from generate_series(
-                  '2015-01-01 12:00am'::date,
-                  '2015-09-23 11:00pm'::date,
-                  '1 hour'::interval) timestamp;''')
+                  select timestamp from generate_series(
+                    '2015-01-01 12:00am'::date,
+                    '2015-09-23 11:00pm'::date,
+                    '1 hour'::interval) timestamp;''')
    conn.commit()
+
+def create_locations_table(conn):
+    cur = conn.cursor()
+    cur.execute("create table Locations (FREGUESIA varchar(35), CONCELHO varchar(22), STAND_ID int, ID serial primary key);")
+    conn.commit()
+
+def fill_locations_table(conn):
+    cur = conn.cursor()
+    cur.execute('''insert into locations (freguesia, concelho)
+                   select freguesia, concelho from map
+                   where distrito = 'PORTO';''')
+    conn.commit()
 
 if __name__ == "__main__":
     conn = psy.connect("dbname=guest user=guest")
+
     create_time_table(conn)
     fill_time_table(conn)
+
+    create_locations_table(conn)
+    fill_locations_table(conn)
